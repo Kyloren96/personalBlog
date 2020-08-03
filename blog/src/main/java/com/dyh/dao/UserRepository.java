aa5852599396785a4e082a2a984c5cc16d96c0b1
dyhfr <fyhfr96@gmail.com>
2020-08-03 13:38:37 +0800

    new version

---
+++ blog/src/main/java/com/dyh/dao/UserRepository.java
@@ -0,0 +1,12 @@
+package com.dyh.dao;
+
+import com.dyh.po.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by limi on 2017/10/15.
+ */
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByUsernameAndPassword(String username, String password);
+}
